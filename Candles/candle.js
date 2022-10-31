e86124e7e9321f7ed7b36a7ce7e2e35eb48ee407
bugapapa <tamasdotbuga@gmail.com>
2022-10-31 22:08:43 +0100

    First  Commit

--- Candles/candle.js
+++ Candles/candle.js
@@ -1,20 +1,12 @@
-const traders = {
-  buyers: 100,
-  sellers: 0,
-  summary: () => {
-    return this.buyers + this.sellers;
-  } 
-} 
 
-function initCandle(){
+function initCandle() {
   const candleBoard = document.getElementById('candleBoard');
-const canvas = document.getElementById('candleCanvas');
-canvas.width = candleBoard.clientWidth;
-canvas.height = candleBoard.clientHeight;
-const canvas_VC = canvas.height / 2;
-const canvas_HC = canvas.width / 2;
+  const canvas = document.getElementById('candleCanvas');
+  canvas.width = candleBoard.clientWidth;
+  canvas.height = candleBoard.clientHeight;
+  const canvas_VC = canvas.height / 2;
 
-const candle = {
+  const candle = {
     x: 3,
     y: canvas_VC,
     width: 10,
@@ -23,59 +15,63 @@ const candle = {
     color: 'red',
     grow: false,
     getWidth: function () {
-        return this.gap + this.width
+      return this.gap + this.width
     }
-}
+  }
 
-let periodTimer = null;
-let period = 5000;
-let candleTimer = null;
-let time = 0;
-let t = 200;
-let buyers = 0;
-let sellers = 0;
+  let periodTimer = null;
+  let period = 5000;
+  let candleTimer = null;
+  let time = 0;
+  let t = 200;
+  let buyers = 0;
+  let sellers = 0;
 
-clearInterval(periodTimer);
+  clearInterval(periodTimer);
 
-periodTimer = setInterval(() => {
+  periodTimer = setInterval(() => {
+
     const ctx = canvas.getContext('2d');
 
     candleTimer = setInterval(() => {
 
-        if (time >= period) {
-            clearInterval(candleTimer);
-        }
+      if (time >= period) {
+        clearInterval(candleTimer);
+      }
 
-        const growing = [true, false];
-        seller = Math.floor(Math.random() * 6);
-        buyer = (Math.floor(Math.random() * 6) * -1);
+      const growing = [true, false];
 
-        candle.grow = growing[Math.floor(Math.random() * growing.length)];
-        candle.height = candle.grow ? candle.height + buyer : candle.height + seller;
-        candle.color = candle.height < 0 ? 'rgb(120, 255, 120)' : 'red';
-        
-        setCandle(candle, canvas, ctx, candle.color);
-        
-        time += t;
-        
+      sellers = createTrade(1);
+      buyers = createTrade(-1);
+
+      candle.grow = growing[Math.floor(Math.random() * growing.length)];
+      candle.height = candle.grow ? candle.height + buyers : candle.height + sellers;
+      candle.color = candle.height < 0 ? 'rgb(120, 255, 120)' : 'red';
+
+      setCandle(candle, canvas, ctx, candle.color);
+
+      time += t;
+
     }, t);
-    
-    traders.sellers = sellers;
-    traders.buyers = buyers;
-    
-    // Set candle properties
-    candle.y += candle.height; 
+
+    candle.y += candle.height;
     candle.x += candle.getWidth();
     candle.height = 0;
 
+  }, period);
 
-}, period);
-
-function setCandle(candle, canvas, context, color) {
+  function setCandle(candle, canvas, context, color) {
     context.clearRect(candle.x, 0, candle.getWidth(), canvas.height);
     context.fillStyle = color;
     context.fillRect(candle.x, candle.y, candle.width, candle.height);
-}
+  }
 
-} 
+  function createTrade(realN){
+    while(true){
+      let n = Math.floor(Math.random() * 6) * realN;
+      if(n !== 0) return n;
+    }
+  }
+
+}
 initCandle();
--- Scores/score.js
+++ Scores/score.js
@@ -1,83 +1,82 @@
 
 
-function initScore(trades){
-  
-  const cash = document.getElementById('userCash');
-  const cashType = document.getElementById('cashType');
-  
-  cash.textContent = 3289;
-  cashType.textContent = '€';
-  
-  const userPoint = trades;
-const maxPoints = 100;
-const angle = 360;
-const userPercent = pointsToPercent(userPoint, maxPoints, true);
-const endAngle = percentToPoints(userPercent, angle, false);
-const scoreText = document.getElementById('scoreText');
+function initScore(trades) {
 
-function pointsToPercent(point, maxPoints, isRound) {
-    return isRound ? Math.round(point / (maxPoints / 100)) : (point / (maxPoints / 100)).toFixed(2);
-}
+    const cash = document.getElementById('userCash');
+    const cashType = document.getElementById('cashType');
 
-function percentToPoints(percent, maxPoints, isRound) {
-    return isRound ? Math.round(percent * (maxPoints / 100)) : (percent * (maxPoints / 100)).toFixed(2);
-}
+    cash.textContent = 3289;
+    cashType.textContent = '€';
 
-function radianToDegree(radians) {
-    return (radians * 180) / Math.PI;
-}
+    const userPoint = trades;
+    const maxPoints = 100;
+    const angle = 360;
+    const userPercent = pointsToPercent(userPoint, maxPoints, true);
+    const endAngle = percentToPoints(userPercent, angle, false);
+    const scoreText = document.getElementById('scoreText');
 
-function degreeToRadian(degrees) {
-    return degrees * (Math.PI / 180);
-}
-
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
-
-canvas.width = 100;
-canvas.height = 100;
-canvas.style.transform = 'rotateZ(-90deg)';
-canvas.style.position = 'absolute';
-document.getElementsByClassName('main-circle')[0].appendChild(canvas);
-
-let id = null;
-let fps = 60;
-let period = 1000 / fps;
-let duration = 3000;
-let time = 0;
-let deg = 0;
-let percent = userPercent < 50 ? 0 : -1;
-let incPercent = userPoint / (duration / period);
-let incDeg = endAngle / (duration / period);
-let incColor = 255 / (duration / period);
-let incColorAngle = (360 / 255);
-let r = 255;
-let g = 0;
-let b = 0;
-
-id = setInterval(() => {
-
-    if(time >= duration){
-        clearInterval(id);
+    function pointsToPercent(point, maxPoints, isRound) {
+        return isRound ? Math.round(point / (maxPoints / 100)) : (point / (maxPoints / 100)).toFixed(2);
     }
 
-    deg += incDeg;
+    function percentToPoints(percent, maxPoints, isRound) {
+        return isRound ? Math.round(percent * (maxPoints / 100)) : (percent * (maxPoints / 100)).toFixed(2);
+    }
 
-    r -= deg >= 180 ? (incColorAngle * 2) : 0;
-    g += incColorAngle;
+    function radianToDegree(radians) {
+        return (radians * 180) / Math.PI;
+    }
 
-    percent += incPercent;
-    scoreText.textContent = Math.round(percent) + '%';
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc((canvas.width / 2), (canvas.height / 2), (canvas.width / 2) - 10, degreeToRadian(0), degreeToRadian(deg), false);
-    ctx.lineWidth = '16';
-    ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
-    ctx.stroke();
+    function degreeToRadian(degrees) {
+        return degrees * (Math.PI / 180);
+    }
 
-    time += period;
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
 
-}, period);
+    canvas.width = 100;
+    canvas.height = 100;
+    canvas.style.transform = 'rotateZ(-90deg)';
+    canvas.style.position = 'absolute';
+    document.getElementsByClassName('main-circle')[0].appendChild(canvas);
+
+    let id = null;
+    let fps = 60;
+    let period = 1000 / fps;
+    let duration = 3000;
+    let time = 0;
+    let deg = 0;
+    let percent = userPercent < 50 ? 0 : -1;
+    let incPercent = userPoint / (duration / period);
+    let incDeg = endAngle / (duration / period);
+    let incColorAngle = (360 / 255);
+    let r = 255;
+    let g = 0;
+    let b = 0;
+
+    id = setInterval(() => {
+
+        if (time >= duration) {
+            clearInterval(id);
+        }
+
+        deg += incDeg;
+
+        r -= deg >= 180 ? (incColorAngle * 2) : 0;
+        g += incColorAngle;
+
+        percent += incPercent;
+        scoreText.textContent = Math.round(percent) + '%';
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc((canvas.width / 2), (canvas.height / 2), (canvas.width / 2) - 10, degreeToRadian(0), degreeToRadian(deg), false);
+        ctx.lineWidth = '10';
+        ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+        ctx.stroke();
+
+        time += period;
+
+    }, period);
 
 } 
